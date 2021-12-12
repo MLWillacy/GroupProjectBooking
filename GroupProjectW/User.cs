4387c483100e181bb8380036c860ffac21879cd0
MLWillacy <71636850+MLWillacy@users.noreply.github.com>
2021-12-12 09:33:33 +0000

    UserClass Loads from file

--- GroupProjectW/User.cs
+++ GroupProjectW/User.cs
@@ -9,13 +9,16 @@ namespace GroupProjectW
 {
     class User
     {
+        string mFilePpath;
         string mEmail;
         string mPassword;
         string[] mSocieties;
-        string[] mBookings;
+        int mNumBookings;
+        List<string> mBookings = new List<string>();
 
         public User(string pFile)
         {
+            mFilePpath = pFile;
             string[] lines = File.ReadAllLines(pFile);
             string[] emailSplit = lines[0].Split(':');
             string[] passwordSplit = lines[1].Split(':');
@@ -23,6 +26,19 @@ namespace GroupProjectW
             mEmail = emailSplit[1];
             mPassword = passwordSplit[1];
 
+            string[] societiesSplit = lines[2].Split(':');
+            mSocieties = societiesSplit[1].Split();
+
+            string[] numBookingsSplit = lines[3].Split(':');
+            mNumBookings = int.Parse(numBookingsSplit[1]);
+
+            int location = 5;
+
+            for (int i = 0; i < mNumBookings; i++)
+            {
+                mBookings.Add(lines[i + location]);
+
+            }
 
         }
     }
